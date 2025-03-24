4264f87663ea2ac6cfc367f4a41c13db234f0a9f
Kaiyue Feng <kf2365@nyu.edu>
2025-03-23 21:42:00 -0700

    clear names

--- offline_evaluation/evaluation.sh
+++ offline_evaluation/evaluation.sh
@@ -4,11 +4,11 @@
 set -e
 
 # Define the full path of the Python script
-PYTHON_SCRIPT="miniconda3/envs/kaiyue/bin/python"
-SCRIPT_PATH="project/Kaiyue/evaluation.py"
+PYTHON_SCRIPT=""
+SCRIPT_PATH=""
 
 # Ensure environment variables and paths are loaded correctly
-export PATH="~/miniconda3/envs/kaiyue/bin:$PATH"
+export PATH=""
 
 # Start the Python script, run in the background, and log output,
 # ensuring it continues execution even if the foreground exits
